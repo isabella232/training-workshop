cb73cfd60f4e1fd242f12136da9959c9b6e338ff
Peter Lanoie <peter.lanoie@octopus.com>
2022-02-09 19:31:55 -0500

    Overall safety check improved, don't need it here

--- admin/testing/provision-student.ps1
+++ admin/testing/provision-student.ps1
@@ -6,15 +6,11 @@ param (
 
 # This script is for testing provisioning a student
 
-if ($baseScriptDir -eq (Get-Location)) {
-	Write-Error "DO NOT RUN THIS FROM IT'S HOME DIRECTORY" exit
-}
-
 $randoStudent = ([System.Guid]:: NewGuid()).ToString().SubString(24)
-. "$baseScriptDir\ ..\provision-student.ps1" `
+. "$PSScriptRoot\..\provision-student.ps1" `
 	-githubUrl $githubUrl -githubPAT $githubPAT `
 	-octopusUrl $octopusURL -octopusKey $octopusKey `
-	-azTenantld $azTenantId -azUser $azUser -azSecret $azSecret `
+	-azTenantId $azTenantId -azUser $azUser -azSecret $azSecret `
 	-skipAzure `
 	-skipGit `
 	-studentName $randoStudent -studentEmail "$randoStudent@octopus.com" `

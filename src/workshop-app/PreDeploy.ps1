0c7aa1aa6daed38e6efc5c52c34ba47ddcd4d895
Peter Lanoie <peter.lanoie@octopus.com>
2022-02-18 20:43:22 -0500

    More excitement

--- src/workshop-app/PreDeploy.ps1
+++ src/workshop-app/PreDeploy.ps1
@@ -4,7 +4,7 @@ if ($null -ne $OctopusParameters) {
 	$studentName = $OctopusParameters["Project:Workshop:StudentName"]
 	# Check that the student name var is present
 	if ($null -ne $studentName) {
-		Write-Highlight "Found a student name: $studentName"
+		Write-Highlight "WOOHOO!! Found a student name: $studentName"
 	}
 } else {
 	Write-Warning "You can't run this script outside of a Octopus."

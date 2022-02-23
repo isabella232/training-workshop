da2e48f4a8242cea7065f0f64ec6947e1fa30e34
Peter Lanoie <peter.lanoie@octopus.com>
2022-02-23 16:37:59 -0500

    mirror teardown order from standup

--- admin/testing/purge-students.ps1
+++ admin/testing/purge-students.ps1
@@ -10,15 +10,17 @@ $users = .$PSScriptRoot\get-users.ps1
 
 $users = $users | Where-Object { $_.DisplayName -like "*Student*" }
 
-Write-Host "Users to purge: $($users.Count)"
-foreach ($user in $users) {
-	Write-Host "Purging student: $($user.DisplayName)"
-	.$PSScriptRoot\delete-user.ps1 -userid $user.id
-}
-
 $spaces = .$PSScriptRoot\get-workshop-spaces.ps1 
-Write-Host "Space to purge: $($spaces.Count)"
+Write-Host "Student spaces to purge: $($spaces.Count)"
 foreach ($space in $spaces) {
 	Write-Host "Purging resources. Slug: $($space.Name)"
 	.$PSScriptRoot\deprovision-student.ps1 -studentSlug $space.Name -skipAzure:$skipAzure -skipOctopus:$skipOctopus
+}
+
+if (!$skipOctopus) {
+	Write-Host "Users to purge: $($users.Count)"
+	foreach ($user in $users) {
+		Write-Host "Purging student: $($user.DisplayName)"
+		.$PSScriptRoot\delete-user.ps1 -userid $user.id
+	}
 }

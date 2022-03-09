a17617a0a7e3d56593c259511319905f1e93e6f0
Peter Lanoie <peter.lanoie@octopus.com>
2022-03-09 11:57:50 -0500

    Add reveal of additional vars

--- src/workshop-app/PreDeploy.ps1
+++ src/workshop-app/PreDeploy.ps1
@@ -6,6 +6,16 @@ if ($null -ne $OctopusParameters) {
 	if ($null -ne $studentName) {
 		Write-Highlight "WOOHOO!! Found a student name: $studentName"
 	}
+	$environmentName = $OctopusParameters["Project:Workshop:Environment"]
+	# Check that the environment name var is present
+	if ($null -ne $environmentName) {
+		Write-Highlight "WOOHOO!! I found the environment name: $environmentName"
+	}
+	$slackUrl = $OctopusParameters["Space:Workshop:SlackUrl"]
+	# Check that the slack var is present
+	if ($null -ne $slackUrl) {
+		Write-Highlight "WOOHOO!! I found the Slack URL variable: $slackUrl"
+	}
 } else {
 	Write-Warning "You can't run this script outside of Octopus."
 }

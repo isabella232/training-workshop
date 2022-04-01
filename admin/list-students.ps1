b2e3d43ac726f5dc91864d1aa6d2999f93552d34
Peter Lanoie <peter.lanoie@octopus.com>
2022-04-01 13:14:36 -0400

    Student info in Azure storage now, get it from there

--- admin/list-students.ps1
+++ admin/list-students.ps1
@@ -1,10 +1,28 @@
+[CmdletBinding()]
+param (
+	[switch]$Local
+)
+
 . "$PSScriptRoot\shared-config.ps1"
 
-$files = Get-ChildItem $dataFolder\*.json 
-# | `
-# 	Select-Object { $_.Name, $_.Name }
-
-foreach ($file in $files) {
-	$slug = $file.Name.Replace(".json","")
+function Write-StudentEntry {
+	param (
+		[string]$slug
+	)
+	$slug = $slug.Replace(".json","")
 	Write-Host "$slug | ..\repo\admin\testing\deprovision-student.ps1 -studentSlug $slug | ..\repo\admin\testing\update-existing-git-branch.ps1 -studentSlug $slug"
 }
+
+if ($Local) {
+	Write-Host "Discovering student list from local files."
+	Get-ChildItem "$dataFolder\*.json" | ForEach-Object { Write-StudentEntry -slug $_.Name }
+} else {
+	Write-Host "Discovering student list from Azure storage."
+	$storageContext = (Get-AzStorageAccount -ResourceGroupName $azResourceGroupName -Name $azStorageAccount).Context
+	Get-AzStorageBlob -Container $azStorageStudentContainer -Context $storageContext | ForEach-Object { Write-StudentEntry -slug $_.Name }
+}
+
+
+
+
+
--- admin/shared-config.ps1
+++ admin/shared-config.ps1
@@ -1,11 +1,15 @@
-$instructionDocsDir = ".\instructions"
-$githubActionsFile = ".\.github\workflows\build-application.yml"
-$dataFolder = "$PSScriptRoot\data"
+$instructionDocsDir = "./instructions"
+$githubActionsFile = "./.github/workflows/build-application.yml"
+$dataFolder = "$PSScriptRoot/data"
 
-$tfOctopusFolder = "$PSScriptRoot\tf-octopus"
-$tfAzureFolder = "$PSScriptRoot\tf-azure"
+$tfOctopusFolder = "$PSScriptRoot/tf-octopus"
+$tfAzureFolder = "$PSScriptRoot/tf-azure"
 
 $azResourceGroupName = "training-workshop"
 $azWebAppServicePlan = "training-workshop-webapps"
 $automationUserId = "Users-23"
 $azLocation = "West US 2"
+
+$azStorageAccount = "trainingworkshopdata"
+$azStorageStudentContainer = "students"
+

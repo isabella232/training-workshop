e4580b3cc5dd4c2648f2c764c92346ada6a06660
Peter Lanoie <peter.lanoie@octopus.com>
2022-04-01 15:28:06 -0400

    Reorg values

--- admin/shared-config.ps1
+++ admin/shared-config.ps1
@@ -2,12 +2,13 @@ $instructionDocsDir = "./instructions"
 $githubActionsFile = "./.github/workflows/build-application.yml"
 $dataFolder = "$PSScriptRoot/data"
 
+$automationUserId = "Users-23"
+
 $tfOctopusFolder = "$PSScriptRoot/tf-octopus"
 $tfAzureFolder = "$PSScriptRoot/tf-azure"
 
 $azResourceGroupName = "training-workshop"
 $azWebAppServicePlan = "training-workshop-webapps"
-$automationUserId = "Users-23"
 $azLocation = "West US 2"
 
 $azStorageAccount = "trainingworkshopdata"

ea14790b5ad5c96e668440c728caec55eaa4b9ff
Peter Lanoie <peter.lanoie@octopus.com>
2022-04-01 14:53:32 -0400

    Missing file for Azure context change

--- admin/delete-student-webapps.ps1
+++ admin/delete-student-webapps.ps1
@@ -1,16 +1,16 @@
 [CmdletBinding()]
 param (
-	[Parameter(Mandatory=$true)] [string] $azTenantId,
-	[Parameter(Mandatory=$true)] [string] $azUser,
-	[Parameter(Mandatory=$true)] [string] $azSecret,
+	# [Parameter(Mandatory=$true)] [string] $azTenantId,
+	# [Parameter(Mandatory=$true)] [string] $azUser,
+	# [Parameter(Mandatory=$true)] [string] $azSecret,
 	[Parameter(Mandatory=$true)] [string] $azResourceGroupName,
 	[Parameter(Mandatory=$true)] [string] $studentSlug
 )
 
-$azSecureSecret = ConvertTo-SecureString -String $azSecret -AsPlainText -Force
-$azCredential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $azUser, $azSecureSecret
+# $azSecureSecret = ConvertTo-SecureString -String $azSecret -AsPlainText -Force
+# $azCredential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $azUser, $azSecureSecret
 
-(Connect-AzAccount -ServicePrincipal -Credential $azCredential -Tenant $azTenantId) | Out-Null
+# (Connect-AzAccount -ServicePrincipal -Credential $azCredential -Tenant $azTenantId) | Out-Null
 
 Write-Host "Looking for existing web apps..."
 $studentWebApps = (Get-AzWebApp -ResourceGroupName $azResourceGroupName) | Where-Object { $_.Name.Contains($studentSlug) }

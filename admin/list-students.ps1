c4e46c822de0cfc6b5e071b0269edb1797a618d5
Peter Lanoie <peter.lanoie@octopus.com>
2022-04-02 01:44:59 -0400

    Refactor to update local cache from blobs

--- admin/list-students.ps1
+++ admin/list-students.ps1
@@ -8,34 +8,37 @@ param (
 . "$PSScriptRoot\shared-octo-utils.ps1"
 . "$PSScriptRoot\shared-config.ps1"
 
-function Write-StudentEntry {
-	param (
-		[string]$slug
-	)
-	$slug = $slug.Replace(".json","")
-	if ($ShortForm) {
-		Write-Host $slug
-		return
-	}
-	Write-Host "$slug | ..\repo\admin\testing\deprovision-student.ps1 -studentSlug $slug | ..\repo\admin\testing\update-existing-git-branch.ps1 -studentSlug $slug"
-}
 
 EnableHighlight
-if ($Local) {
-	Write-Host "Discovering student list from local files."
-	$studentItems = Get-ChildItem "$dataFolder\*.json" # | ForEach-Object { Write-StudentEntry -slug $_.Name }
-} else {
+if (!$Local) {
 	Write-Host "Discovering student list from Azure storage."
 	$storageContext = (Get-AzStorageAccount -ResourceGroupName $azResourceGroupName -Name $azStorageAccount).Context
 	$blobItems = Get-AzStorageBlob -Container $azStorageStudentContainer -Context $storageContext # | ForEach-Object { Write-StudentEntry -slug $_.Name }
 
-	if ($ForBlobInfo) {
-		foreach ($item in $blobItems) {
-			Write-Host "($($item.Length)) Get-AzStorageBlob -Container $azStorageStudentContainer -Context `$storageContext -Blob ""$($item.Name)"""
+	foreach ($item in $blobItems) {
+		$filePath = "$dataFolder\$($item.Name)"
+		$slug = $item.Name.Replace(".json", "")
+		if (!(Test-Path "$filePath")) {
+			Write-Host "Getting blob file for '$($item.Name)'..."
+			Get-AzStorageBlobContent -Container $azStorageStudentContainer -Context $storageContext -Blob $item.Name -Destination $filePath
 		}
-		Write-Host "Don't forget: `$storageContext = (Get-AzStorageAccount -ResourceGroupName $azResourceGroupName -Name $azStorageAccount).Context"
-		$studentItems = $null
-	} 
+	}
 }
-$studentItems | ForEach-Object { Write-StudentEntry -slug $_.Name }
+
+$studentFiles = Get-ChildItem "$dataFolder\*.json"
+
+foreach ($file in $studentFiles) {
+	$slug = $file.Name.Replace(".json", "")
+	$studentInfo = Get-Content $file | ConvertFrom-Json
+	$switches = ""
+	if (!$studentInfo.AzureApps[0].ResourceId -or $studentInfo.AzureApps[0].ResourceId.Length -eq 0) {
+		$switches = "-skipAzure -forceCleanup "
+	}
+	Write-Host "..\repo\admin\testing\deprovision-student.ps1 -studentSlug $slug $switches| ..\repo\admin\testing\update-existing-git-branch.ps1 -studentSlug $slug | ($($file.Length))"
+
+	if ($ShortForm) {
+		Write-Host $slug
+		return
+	}
+}
 DisableHighlight
